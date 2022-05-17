b3712320aec13f48893853b4c5abd00ade1b8bdf
Ben Jones <ben.jones12@gmail.com>
2022-05-17 15:31:17 +1200

    add githubUrl to firmware adoption payload

--- src/main.cpp
+++ src/main.cpp
@@ -138,10 +138,14 @@ void getFirmwareJson(JsonVariant json)
 {
   JsonObject firmware = json.createNestedObject("firmware");
 
-  firmware["name"] = STRINGIFY(FW_NAME);
-  firmware["shortName"] = STRINGIFY(FW_SHORT_NAME);
-  firmware["maker"] = STRINGIFY(FW_MAKER);
+  firmware["name"] = FW_NAME;
+  firmware["shortName"] = FW_SHORT_NAME;
+  firmware["maker"] = FW_MAKER;
   firmware["version"] = STRINGIFY(FW_VERSION);
+
+#if defined(FW_GITHUB_URL)
+  firmware["githubUrl"] = FW_GITHUB_URL;
+#endif
 }
 
 void getSystemJson(JsonVariant json)
@@ -183,7 +187,7 @@ void getConfigSchemaJson(JsonVariant json)
   
   // Config schema metadata
   configSchema["$schema"] = JSON_SCHEMA_VERSION;
-  configSchema["title"] = STRINGIFY(FW_SHORT_NAME);
+  configSchema["title"] = FW_SHORT_NAME;
   configSchema["type"] = "object";
 
   JsonObject properties = configSchema.createNestedObject("properties");
@@ -199,7 +203,7 @@ void getCommandSchemaJson(JsonVariant json)
   
   // Command schema metadata
   commandSchema["$schema"] = JSON_SCHEMA_VERSION;
-  commandSchema["title"] = STRINGIFY(FW_SHORT_NAME);
+  commandSchema["title"] = FW_SHORT_NAME;
   commandSchema["type"] = "object";
 
   JsonObject properties = commandSchema.createNestedObject("properties");
